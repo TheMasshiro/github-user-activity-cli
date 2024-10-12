1de76a905218920e2c21c5ea2136858ca33da185
John Christian Vicente <johnc.vicente1@gmail.com>
2024-10-12 10:57:20 +0800

    refactor: restructure event handling for clarity and efficiency

--- github_activity_api.py
+++ github_activity_api.py
@@ -4,7 +4,7 @@ import socket
 import sys
 from abc import ABC, abstractmethod
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Any, Optional
 
 
 class AbstractAPI(ABC):
@@ -127,7 +127,7 @@ class RateLimit:
                 "%I:%M:%S %p | %B %d, %Y"
             )
 
-            print("Overall API Request Limit:")
+            print("Overall API Request Usage:")
             print(f"  Used:        {used}")
             print(f"  Usage:       {remaining}/{limit}")
             print(f"  Reset at:    {reset_time}")
@@ -150,7 +150,7 @@ class UserActivity:
         endpoint = f"/users/{self.username}/events"
         if self.rate.handle_api_limit() < 1:
             self.api_endpoint.handle_error(
-                "Error: API request limit exceeded. Please wait and try again later\nRun --limit or -l to check the time remaining until your request limit resets\n"
+                "Error: API request limit exceeded. Please wait and try again later\nRun --usage or -u to check the time remaining until your request limit resets\n"
             )
         return self.api_endpoint.get_content(endpoint, self.username)
 
@@ -159,40 +159,73 @@ class UserActivity:
             return "NoEvent"
 
         event_type = event_type.lower()
-        event_list = ["push", "pull", "star", "issues"]
+        github_events = {
+            "push": "PushEvent",
+            "pull": "PullRequestEvent",
+            "star": "WatchEvent",
+            "issues": "IssuesEvent",
+            "fork": "ForkEvent",
+            "delete": "DeleteEvent",
+            "comment": "IssueCommentEvent",
+            "create": "CreateEvent",
+        }
 
-        if event_type == event_list[0]:
-            return "PushEvent"
-        elif event_type == event_list[1]:
-            return "PullRequestEvent"
-        elif event_type == event_list[2]:
-            return "WatchEvent"
-        elif event_type == event_list[3]:
-            return "IssuesEvent"
-        elif event_type not in event_list:
+        for key, value in github_events.items():
+            if event_type == key:
+                return value
+
+        if event_type not in github_events:
             return "NoEvent"
 
         return None
 
-    def event_message(self, event: str, commit_count: int, repo_name: str):
-        if event == "PushEvent":
-            if commit_count == 1:
-                return f"- Pushed {commit_count} commit to {repo_name}"
-            return f"- Pushed {commit_count} commits to {repo_name}"
-        elif event == "PullRequestEvent":
-            return f"- Opened a pull request in {repo_name}"
-        elif event == "WatchEvent":
-            return f"- Starred {repo_name}"
-        elif event == "IssuesEvent":
-            return f"- Opened a new issue in {repo_name}"
+    def event_message(
+        self,
+        event_type: str,
+        commit_size: int,
+        repo_name: str,
+        ref: Optional[dict] = None,
+    ):
+        github_messages = {
+            "PushEvent": f"- Pushed {commit_size} commit{'' if commit_size == 1 else 's'} to {repo_name}",
+            "PullRequestEvent": f"- Opened a pull request in {repo_name}",
+            "WatchEvent": f"- Starred {repo_name}",
+            "IssuesEvent": f"- Opened a new issue in {repo_name}",
+            "ForkEvent": f"- Forked {repo_name}",
+            "IssueCommentEvent": f"- Commented on an issue in {repo_name}",
+        }
+
+        for event, message in github_messages.items():
+            if event_type == event:
+                return message
+
+        if ref is None:
+            return ""
+
+        for event, ref_info in ref.items():
+            if event != event_type:
+                continue
+
+            for event, ref_info in ref.items():
+                if event != event_type or event not in ["CreateEvent", "DeleteEvent"]:
+                    continue
+
+                for ref_name, ref_type in ref_info.items():
+                    if ref_type == "repository":
+                        return f"- Created a new repository {repo_name}"
+                    elif ref_type == "branch":
+                        event_action = (
+                            "Created a new" if event == "CreateEvent" else "Deleted a"
+                        )
+                        return f"- {event_action} branch {ref_name}"
+
         return ""
 
-    def handle_response(self, parser, event: Optional[str] = None):
-        repos = {}
-        repo_dates = {}
+    def handle_response(self, parser, user_event: Optional[str] = None):
+        grouped_events = {}
+        event_ref: dict[Any, Any] = {}
 
         responses = self.__response()
-
         if isinstance(responses, dict) and (
             responses.get("status") == "404" or responses.get("message") == "Not Found"
         ):
@@ -200,50 +233,71 @@ class UserActivity:
 
         for response in responses:
             event_type = response.get("type")
-            repo_name = response.get("repo", {}).get("name")
             event_date = response.get("created_at")
+            repo_name = response.get("repo", {}).get("name")
+            payload_size = response.get("payload", {}).get("size")
+            payload_ref_type = response.get("payload", {}).get("ref_type")
+            payload_ref = response.get("payload", {}).get("ref")
+            pull_request = response.get("payload", {}).get("pull_request", {}).get("id")
 
-            if not repo_name:
+            if not repo_name or not event_date:
                 continue
 
-            if repo_name not in repos:
-                repos[repo_name] = {
+            github_date = datetime.strptime(event_date, "%Y-%m-%dT%H:%M:%SZ")
+            format_date = github_date.strftime("%B %d, %Y")
+
+            if payload_size is None or pull_request is not None:
+                payload_size = 1
+
+            key = (format_date, repo_name)
+            if key not in grouped_events:
+                grouped_events[key] = {
                     "PushEvent": 0,
                     "PullRequestEvent": 0,
                     "IssuesEvent": 0,
                     "WatchEvent": 0,
+                    "ForkEvent": 0,
+                    "DeleteEvent": 0,
+                    "IssueCommentEvent": 0,
+                    "CreateEvent": 0,
                 }
-                repo_dates[repo_name] = event_date
 
-            if event_type in repos[repo_name]:
-                repos[repo_name][event_type] += 1
+            grouped_events[key][event_type] += payload_size
 
-            if event_date > repo_dates[repo_name]:
-                repo_dates[repo_name] = event_date
+            if event_type in ["CreateEvent", "DeleteEvent"]:
+                if key not in event_ref:
+                    event_ref[key] = {}
+                if event_type not in event_ref[key]:
+                    event_ref[key][event_type] = {}
+                event_ref[key][event_type] = {payload_ref: payload_ref_type}
 
-        filtered_event = self.handle_event(event) if event else None
+        filtered_event = self.handle_event(user_event) if user_event else None
+        format_messages: dict[Any, Any] = {}
 
-        output_messages = []
-
-        for repo_name, events in repos.items():
-            date = datetime.strptime(repo_dates[repo_name], "%Y-%m-%dT%H:%M:%SZ")
-            format_date = date.strftime("%B %d, %Y")
-
-            for event_type, count in events.items():
+        for (date, repo), events in grouped_events.items():
+            for event, commit_size in events.items():
                 if filtered_event != "NoEvent":
-                    event_msg = self.event_message(event_type, count, repo_name)
+                    event_ref_key = (date, repo) if (date, repo) in event_ref else None
+                    messages = self.event_message(
+                        event, commit_size, repo, event_ref.get(event_ref_key)
+                    )
 
-                    if (event_type == filtered_event and count > 0) or (
-                        filtered_event is None and count > 0
-                    ):
-                        if event_msg:
-                            output_messages.append(f"{event_msg}")
-                            output_messages.append(f"  Latest activity: {format_date}")
-                            output_messages.append("")
+                    if (
+                        (event == filtered_event) or (filtered_event is None)
+                    ) and commit_size > 0:
+                        if messages:
+                            format_messages.setdefault(date, []).append(f"{messages}")
 
         if filtered_event == "NoEvent":
-            print(f"Error: {event} is an invalid event type")
+            print(f"Error: '{user_event}' is an invalid event type")
             if parser:
                 parser.print_help()
             sys.exit(0)
+
+        output_messages = []
+        for date, messages in format_messages.items():
+            output_messages.append(date)
+            output_messages.extend(messages)
+            output_messages.append("")
+
         return output_messages
